f11b58e8a41a177027f2014ce62f3d861674eb2c
pavani701 <pavaniakula154@gmail.com>
2024-08-24 20:05:25 +0530

    ui

---
+++ js/javascript.js
@@ -0,0 +1,7 @@
+let otv=true;
+if(otv==true){
+    console.log("we cannot forward")
+}
+else{
+console.log("we can forward")
+}
---
+++ js/new.js
@@ -0,0 +1,12 @@
+let a=5;
+let b=6;
+let c=15;
+if(a>=b&&a>=c){
+    console.log(a);
+    else(b>=c&&b>==a){
+        console.log(b);
+    }
+}
+else
+ console.log(c);
+
